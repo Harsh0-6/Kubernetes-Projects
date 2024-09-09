1875ed103e566031ce919b6076a9c078ab8ba906
Harshvardhan Singh <harshvardhansingh2306@gmail.com>
2024-09-09 23:55:37 +0530

    adding express http server with k8s

---
+++ index.js
@@ -0,0 +1,16 @@
+/* simple backend express http server, adding dockerFile to containerize it in node.js 
+    and then deploy it on kubernetes cluster using Minikube.
+ */
+const express = require('express');  
+const port = 3000;  
+const app = express(); 
+
+
+// Define a route for the root URL
+app.get('/', function(req, res){
+    res.send('Hello World!')     
+
+})
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`); 
+});
